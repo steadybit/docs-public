19b317d084d3eb7e7305f110cca7f04c4635166e
Benjamin Wilms <benjamin@chaosmesh.com>
2020-02-06 11:51:19 +0100

    added chaosmesh default footer to docs

---
+++ src/components/footer/index.js
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2019 chaosmesh GmbH. All rights reserved.
+ */
+
+import React from 'react';
+import './styles.css';
+
+export default function () {
+  return (
+    <footer className="cm-footer">
+      <div className="cm-inside">
+             <span className="cm-copyright">Need Help? Get in touch <a href="https://www.chaosmesh.com/contact/">with us</a></span>
+        <span className="cm-copyright">© chaosmesh. All rights reserved.</span>
+      </div>
+    </footer>
+  );
+}
