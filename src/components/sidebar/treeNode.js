df7de18bd6fbbdab53e8d48fd4594d0b0a7cc0ad
Manuel Gerding <manuel@steadybit.com>
2021-03-05 11:40:17 +0100

    feat: Adding collapsible NavItems

--- src/components/sidebar/treeNode.js
+++ src/components/sidebar/treeNode.js
@@ -1,23 +1,55 @@
 /*
- * Copyright 2020 steadybit GmbH. All rights reserved.
+ * Copyright 2021 steadybit GmbH. All rights reserved.
  */
 
 import React from "react";
-import config from '../../../config';
+import config from "../../../config";
 import Link from "../link";
+import { AnimatePresence, motion } from "framer-motion";
+import iconChevronRight from "../../assets/images/icon-chevron-right.svg";
+import iconChevronDown from "../../assets/images/icon-chevron-down.svg";
 
-const TreeNode = ({className = '', url, title, items}) => {
+const TreeNode = ({ className = "", url, title, items }) => {
+  const [isHidden, setHidden] = React.useState(true);
   const hasChildren = items.length !== 0;
-  const location = typeof document !== 'undefined' ? document.location : undefined;
-  const active = location && (location.pathname === url || location.pathname === (config.gatsby.pathPrefix + url));
-  const calculatedClassName = `${className} item ${active ? 'active' : ''}`;
+  const location =
+    typeof document !== "undefined" ? document.location : undefined;
+  const active =
+    location &&
+    (location.pathname === url ||
+      location.pathname === config.gatsby.pathPrefix + url);
+  const calculatedClassName = `${className} item ${active ? "active" : ""}`;
   return (
     <li className={calculatedClassName}>
-      {title ? (<Link to={url}>{title}</Link>) : null}
+      {title ? (
+        <Link to={url} onClick={() => setHidden(!isHidden)}>
+          {title}
+          {hasChildren ? (
+            <img
+              src={isHidden ? iconChevronRight : iconChevronDown}
+              alt={isHidden ? "chevron right" : "chevron down"}
+            />
+          ) : null}
+        </Link>
+      ) : null}
       {hasChildren ? (
-        <ul>
-          {items.map((item) => (<TreeNode key={item.url} {...item} />))}
-        </ul>
+        <AnimatePresence initial={false}>
+          {!isHidden ? (
+            <motion.div
+              style={{ overflow: "hidden" }}
+              initial={{ height: 0 }}
+              animate={{ height: "auto" }}
+              exit={{ height: 0 }}
+              as={"ul"}
+            >
+              <ul style={{ display: isHidden ? "none" : "block" }}>
+                {items.map((item) => (
+                  <TreeNode key={item.url} {...item} />
+                ))}
+              </ul>
+            </motion.div>
+          ) : null}
+        </AnimatePresence>
       ) : null}
     </li>
   );
