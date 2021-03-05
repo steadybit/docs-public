d83f8a854d3f5e3941082bc62a40a89ef36bda1c
Manuel Gerding <manuel@steadybit.com>
2021-03-05 17:22:16 +0100

    feat: Supporting order in SubNav

--- config.js
+++ config.js
@@ -3,19 +3,31 @@
  */
 
 const config = {
-  "gatsby": {
-    "pathPrefix": "",
-    "siteUrl": "https://docs.steadybit.io",
-    "gaTrackingId": "UA-145692204-1"
+  gatsby: {
+    pathPrefix: "",
+    siteUrl: "https://docs.steadybit.io",
+    gaTrackingId: "UA-145692204-1",
   },
-  "sidebar": {
-    "forcedNavOrder": ['getting-started', 'experiments', 'attacks', 'discovery', 'execution-monitoring', 'areas', 'teams-and-users', 'integration', 'architecture', 'installation-agent', 'installation-platform', 'release-notes'],
-    "frontline": false,
-    "ignoreIndex": true,
+  sidebar: {
+    forcedNavOrder: [
+      "setup-configure",
+      "use",
+      "learn",
+      "integrate",
+      "release-notes",
+    ],
+    forcedSubNavOrder: {
+      "setup-configure": ["configure-monitoring", "areas"],
+      use: [],
+      learn: [],
+      integrate: [],
+    },
+    frontline: false,
+    ignoreIndex: true,
   },
-  "siteMetadata": {
-    "title": "steadybit Documentation",
-    "description": "steadybit Chaos Engineering Platform Documentation"
+  siteMetadata: {
+    title: "steadybit Documentation",
+    description: "steadybit Chaos Engineering Platform Documentation",
   },
 };
 
--- src/components/sidebar/tree.js
+++ src/components/sidebar/tree.js
@@ -1,81 +1,114 @@
 /*
- * Copyright 2020 steadybit GmbH. All rights reserved.
+ * Copyright 2021 steadybit GmbH. All rights reserved.
  */
 
-import React, {useState} from 'react';
-import config from '../../../config';
-import TreeNode from './treeNode';
+import React, { useState } from "react";
+import config from "../../../config";
+import TreeNode from "./treeNode";
 
-const calculateTreeData = edges => {
-  const originalData = config.sidebar.ignoreIndex ? edges.filter(({node: {fields: {slug}}}) => slug !== '/') : edges;
-  const tree = originalData.reduce((accu, {node: {fields: {slug, title}, frontmatter: {navTitle}}}) => {
-    const parts = slug.split('/');
-    let {items: prevItems} = accu;
-    for (const part of parts.slice(1, -1)) {
-      let tmp = prevItems.find(({label}) => label === part);
-      if (tmp) {
-        if (!tmp.items) {
-          tmp.items = [];
-        }
-      } else {
-        tmp = {label: part, items: []};
-        prevItems.push(tmp);
+const calculateTreeData = (edges) => {
+  const originalData = config.sidebar.ignoreIndex
+    ? edges.filter(
+        ({
+          node: {
+            fields: { slug },
+          },
+        }) => slug !== "/"
+      )
+    : edges;
+  const tree = originalData.reduce(
+    (
+      accu,
+      {
+        node: {
+          fields: { slug, title },
+          frontmatter: { navTitle },
+        },
       }
-      prevItems = tmp.items;
-    }
-    const existingItem = prevItems.find(({label}) => label === parts[parts.length - 1]);
-    if (existingItem) {
-      existingItem.url = slug;
-      existingItem.title = navTitle || title;
-    } else {
-      prevItems.push({
-        label: parts[parts.length - 1],
-        url: slug,
-        items: [],
-        title: navTitle || title
-      });
-    }
-    return accu;
-  }, {items: []});
-  const {sidebar: {forcedNavOrder = []}} = config;
+    ) => {
+      const parts = slug.split("/");
+      let { items: prevItems } = accu;
+      for (const part of parts.slice(1, -1)) {
+        let tmp = prevItems.find(({ label }) => label === part);
+        if (tmp) {
+          if (!tmp.items) {
+            tmp.items = [];
+          }
+        } else {
+          tmp = { label: part, items: [] };
+          prevItems.push(tmp);
+        }
+        prevItems = tmp.items;
+      }
+      const existingItem = prevItems.find(
+        ({ label }) => label === parts[parts.length - 1]
+      );
+      if (existingItem) {
+        existingItem.url = slug;
+        existingItem.title = navTitle || title;
+      } else {
+        prevItems.push({
+          label: parts[parts.length - 1],
+          url: slug,
+          items: [],
+          title: navTitle || title,
+        });
+      }
+      return accu;
+    },
+    { items: [] }
+  );
+  const {
+    sidebar: { forcedNavOrder = [], forcedSubNavOrder = [] },
+  } = config;
   const reversedNavOrder = [...forcedNavOrder];
   reversedNavOrder.reverse();
   return reversedNavOrder.reduce((accu, slug) => {
-    const parts = slug.split('/');
-    let {items: prevItems} = accu;
+    const parts = slug.split("/");
+    let { items: prevItems } = accu;
     for (const part of parts.slice(1, -1)) {
-      let tmp = prevItems.find(({label}) => label === part);
+      let tmp = prevItems.find(({ label }) => label === part);
       if (tmp) {
         if (!tmp.items) {
           tmp.items = [];
         }
       } else {
-        tmp = {label: part, items: []};
+        tmp = { label: part, items: [] };
         prevItems.push(tmp);
       }
       prevItems = tmp.items;
     }
     // sort items alphabetically.
     prevItems.map((item) => {
-      item.items = item.items
-        .sort(function (a, b) {
-          if (a.label < b.label)
+      item.items = item.items.sort(function (a, b) {
+        const forcedOrder = forcedSubNavOrder[item.label];
+        if (
+          forcedOrder &&
+          forcedOrder.indexOf(a.label) >= 0 &&
+          forcedOrder.indexOf(b.label) >= 0
+        ) {
+          if (forcedOrder.indexOf(a.label) > forcedOrder.indexOf(b.label))
             return -1;
-          if (a.label > b.label)
+          if (forcedOrder.indexOf(a.label) < forcedOrder.indexOf(b.label))
             return 1;
           return 0;
-        });
+        }
+        if (a.label < b.label) return -1;
+        if (a.label > b.label) return 1;
+        return 0;
+      });
     });
-    const index = prevItems.findIndex(({label}) => label === parts[parts.length - 1]);
+    const index = prevItems.findIndex(
+      ({ label }) => label === parts[parts.length - 1]
+    );
     accu.items.unshift(prevItems.splice(index, 1)[0]);
     return accu;
   }, tree);
 };
 
-
-const Tree = ({edges}) => {
+const Tree = ({ edges }) => {
   const [rootNode] = useState(() => calculateTreeData(edges));
-  return rootNode.items.map(item => <TreeNode key={item.url} {...item} />);
+  return rootNode.items.map((item) => <TreeNode key={item.url} {...item} />);
 };
 
 export default Tree;
