c308b1bd1e5921deaa8cbe43c42161a1eba36bd0
Manuel Gerding <manuel@steadybit.com>
2021-04-28 17:58:59 +0200

    fix: Replacing GatsbyLink with Gatsby-navigate to prevent pre-fetching of page-data.jsons (leading to HTTP 429 too many requests in deployed environment)

--- src/components/link.js
+++ src/components/link.js
@@ -1,12 +1,26 @@
-import {Link as GatsbyLink} from "gatsby";
+/*
+ * Copyright 2021 steadybit GmbH. All rights reserved.
+ */
+
+import { navigate } from "gatsby";
 import isAbsoluteUrl from "is-absolute-url";
 import React from "react";
 
-const Link = ({to, ...props}) =>
+const Link = ({ to, ...props }) =>
   isAbsoluteUrl(to) ? (
     <a href={to} {...props} />
   ) : (
-    <GatsbyLink to={to} {...props} />
+    <a
+      {...props}
+      style={{ cursor: "pointer", ...(props.style ? props.style : {}) }}
+      onClick={() => {
+        if (props.onClick) {
+          props.onClick();
+        }
+        navigate(to);
+        return false;
+      }}
+    />
   );
 
 export default Link;
